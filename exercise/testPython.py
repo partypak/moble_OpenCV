656fa8fbe6a26aab0c4ea9304cf4b998675c6391
partypak <danbalove@naver.com>
2022-09-07 18:44:16 +0900

    텍스트 추출

--- exercise/testPython.py
+++ exercise/testPython.py
@@ -1,38 +1,64 @@
-# 0207.py
 import cv2
-import numpy as np
-
-##cap = cv2.VideoCapture(0)  # 0번 카메라
-cap = cv2.VideoCapture('./data/vtest.avi')
-##cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-##cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-
-frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-              int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-print('frame_size =', frame_size)
-imageFile = './data/lena.jpg'
-
-lena = cv2.imread(imageFile) 
-ratio1 = 0.6
-ratio2 = 0.4
-
-while True:   
-    retval, frame = cap.read()
-    if not retval:
-        break
-    
-    lena =cv2.resize(lena,(640,480))
-    frame =cv2.resize(frame,(640,480))
-    text = 'OpenCV Programming'
-    org = (100,470)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame,text, org, font, 1, (255,0,0), 2)
-    add=cv2.addWeighted(frame,0.9,lena,1,5)
-    cv2.imshow('frame',add)
-    
-    key = cv2.waitKey(25)
-    if key == 27: # Esc
-        break
-if cap.isOpened():
-    cap.release()
-cv2.destroyAllWindows()
+import pytesseract
+ 
+# Mention the installed location of Tesseract-OCR in your system
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+ 
+# Read image from which text needs to be extracted
+img = cv2.imread('./data/sample2.jpg')
+ 
+# Preprocessing the image starts
+ 
+# Convert the image to gray scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+# Performing OTSU threshold
+ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+ 
+# Specify structure shape and kernel size.
+# Kernel size increases or decreases the area
+# of the rectangle to be detected.
+# A smaller value like (10, 10) will detect
+# each word instead of a sentence.
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+ 
+# Applying dilation on the threshold image
+dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+ 
+# Finding contours
+contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                 cv2.CHAIN_APPROX_NONE)
+ 
+# Creating a copy of image
+im2 = img.copy()
+ 
+# A text file is created and flushed
+file = open("recognized.txt", "w+")
+file.write("")
+file.close()
+ 
+# Looping through the identified contours
+# Then rectangular part is cropped and passed on
+# to pytesseract for extracting text from it
+# Extracted text is then written into the text file
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+     
+    # Drawing a rectangle on copied image
+    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+     
+    # Cropping the text block for giving input to OCR
+    cropped = im2[y:y + h, x:x + w]
+     
+    # Open the file in append mode
+    file = open("recognized.txt", "a")
+     
+    # Apply OCR on the cropped image
+    text = pytesseract.image_to_string(cropped)
+     
+    # Appending the text into file
+    file.write(text)
+    file.write("\n")
+     
+    # Close the file
+    file.close
